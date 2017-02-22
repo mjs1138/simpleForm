4626ff07e6d43548d571a6b3cab76ed1e8a84b4f
Mike <mike@scllc.us>
2017-02-22 16:35:18 -0500

    added a drupal 8 redirect to submit()

--- src/Form/SimpleForm.php
+++ src/Form/SimpleForm.php
@@ -5,6 +5,7 @@ namespace Drupal\simpleForm\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url; //????
 
 /**
  * Implements the SimpleForm form controller.
@@ -14,98 +15,110 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @see \Drupal\Core\Form\FormBase
  */
-class SimpleForm extends FormBase {
+class SimpleForm extends FormBase
+{
 
-  /**
-   * Build the simple form.
-   *
-   * A build form method constructs an array that defines how markup and
-   * other form elements are included in an HTML form.
-   *
-   * @param array $form
-   *   Default form array structure.
-   * @param FormStateInterface $form_state
-   *   Object containing current form state.
-   *
-   * @return array
-   *   The render array defining the elements of the form.
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-
-    $form['title'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Title'),
-      '#description' => $this->t('Title must be at least 5 characters in length.'),
-      '#required' => TRUE,
-    ];
-
-    // Group submit handlers in an actions element with a key of "actions" so
-    // that it gets styled correctly, and so that other modules may add actions
-    // to the form. This is not required, but is convention.
-    $form['actions'] = [
-      '#type' => 'actions',
-    ];
-
-    // Add a submit button that handles the submission of the form.
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
-    ];
-
-    return $form;
-  }
-
-  /**
-   * Getter method for Form ID.
-   *
-   * The form ID is used in implementations of hook_form_alter() to allow other
-   * modules to alter the render array built by this form controller.  it must
-   * be unique site wide. It normally starts with the providing module's name.
-   *
-   * @return string
-   *   The unique ID of the form defined by this class.
-   */
-  public function getFormId() {
-    return 'simpleForm_simple_form';
-  }
-
-  
-  /**
-   * Implements form validation.
-   *
-   * The validateForm method is the default method called to validate input on
-   * a form.
-   *
-   * @param array $form
-   *   The render array of the currently built form.
-   * @param FormStateInterface $form_state
-   *   Object describing the current state of the form.
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    $title = $form_state->getValue('title');
-    if (strlen($title) < 5) {
-      // Set an error for the form element with a key of "title".
-      $form_state->setErrorByName('title', $this->t('The title must be at least 5 characters long.'));
-    }
-  }
-
-  /**
-   * Implements a form submit handler.
-   *
-   * The submitForm method is the default method called for any submit elements.
-   *
-   * @param array $form
-   *   The render array of the currently built form.
-   * @param FormStateInterface $form_state
-   *   Object describing the current state of the form.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    /*
-     * This would normally be replaced by code that actually does something
-     * with the title.
+    /**
+     * Build the simple form.
+     *
+     * A build form method constructs an array that defines how markup and
+     * other form elements are included in an HTML form.
+     *
+     * @param array $form
+     *   Default form array structure.
+     * @param FormStateInterface $form_state
+     *   Object containing current form state.
+     *
+     * @return array
+     *   The render array defining the elements of the form.
      */
-    $title = $form_state->getValue('title');
-    drupal_set_message(t('You specified a title of %title.', ['%title' => $title]));
-  }
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+
+        $form['title'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Title'),
+            '#description' => $this->t('Title must be at least 5 characters in length.'),
+            '#required' => TRUE,
+        ];
+
+        // Group submit handlers in an actions element with a key of "actions" so
+        // that it gets styled correctly, and so that other modules may add actions
+        // to the form. This is not required, but is convention.
+        $form['actions'] = [
+            '#type' => 'actions',
+        ];
+
+        // Add a submit button that handles the submission of the form.
+        $form['actions']['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Submit'),
+        ];
+
+        return $form;
+    }
+
+    /**
+     * Getter method for Form ID.
+     *
+     * The form ID is used in implementations of hook_form_alter() to allow other
+     * modules to alter the render array built by this form controller.  it must
+     * be unique site wide. It normally starts with the providing module's name.
+     *
+     * @return string
+     *   The unique ID of the form defined by this class.
+     */
+    public function getFormId()
+    {
+        return 'simpleForm_simple_form';
+    }
+
+
+    /**
+     * Implements form validation.
+     *
+     * The validateForm method is the default method called to validate input on
+     * a form.
+     *
+     * @param array $form
+     *   The render array of the currently built form.
+     * @param FormStateInterface $form_state
+     *   Object describing the current state of the form.
+     */
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        $title = $form_state->getValue('title');
+        if (strlen($title) < 5) {
+            // Set an error for the form element with a key of "title".
+            $form_state->setErrorByName('title', $this->t('The title must be at least 5 characters long.'));
+        }
+    }
+
+    /**
+     * Implements a form submit handler.
+     *
+     * The submitForm method is the default method called for any submit elements.
+     *
+     * @param array $form
+     *   The render array of the currently built form.
+     * @param FormStateInterface $form_state
+     *   Object describing the current state of the form.
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        /*
+         * This would normally be replaced by code that actually does something
+         * with the title.
+         */
+        $title = $form_state->getValue('title');
+
+ //       $form_state['redirect'] = 'mjs1138/simpleForm/simple-form';
+ //       $form_state['redirect'] = new Url('mjs1138/simpleForm/simple-form');
+ //       $form_state->setRedirectUrl('http://localhost/drupal-8.2.6/mjs1138/simpleForm/simple-form');
+        $url = Url::fromRoute('simpleForm.description');
+        $form_state->setRedirectUrl($url);
+
+        drupal_set_message(t('You specified a title of %title.', ['%title' => $title]));
+    }
 
 }
