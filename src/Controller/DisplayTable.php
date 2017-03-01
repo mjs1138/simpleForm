88715502e6644193412cd4220f0e179c93593134
Mike <mike@scllc.us>
2017-03-01 16:01:39 -0500

    More Table examples

--- src/Controller/DisplayTable.php
+++ src/Controller/DisplayTable.php
@@ -3,50 +3,45 @@
 namespace Drupal\simpleForm\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Link;
 
 /**
  * Simple page controller for drupal.
  */
-class DisplayTable extends ControllerBase {
+class DisplayTable extends ControllerBase
+{
 
-  /**
-   * Lists the examples provided by form_example.
-   */
-  public function description_one() {
+    public function description_one()
+    {
+
+        $query = \Drupal::database()->select('users_field_data', 'u');
+        $query->fields('u', ['uid', 'name', 'mail']);
+        $results = $query->execute()->fetchAll();
+
+        $header = [
+            'userid' => t('User id'),
+            'Username' => t('username'),
+            'email' => t('Email'),
+        ];
+
+        // Populate the rows.
+        $rows = array();
+        foreach ($results as $row) {
+            $rows[] = array('data' => array(
+                'userid' => $row->uid,     // 'userid' was the key used in the header
+                'Username' => $row->name, // 'Username' was the key used in the header
+                'email' => $row->mail,    // 'email' was the key used in the header
+            ));
+        }
+
+        $form['table'] = [
+            '#type' => 'table',
+            '#header' => $header,
+            '#rows' => $rows,
+            '#empty' => t('No users found!!!'),
+        ];
 
 
-      $query = \Drupal::database()->select('users_field_data', 'u');
-      $query->fields('u', ['uid','name','mail']);
-      $results = $query->execute()->fetchAll();
-
-      $header = [
-          'userid' => t('User id'),
-          'Username' => t('username'),
-          'email' => t('Email'),
-      ];
-
-      // Initialize an empty array
-      $output = array();
-// Next, loop through the $results array
-      foreach ($results as $result) {
-//          if ($result->uid != 0 && $result->uid != 1) {
-              $output[$result->uid] = [
-                  'userid' => $result->uid,     // 'userid' was the key used in the header
-                  'Username' => $result->name, // 'Username' was the key used in the header
-                  'email' => $result->mail,    // 'email' was the key used in the header
-              ];
-//          }
-      }
-
-      $form['table'] = [
-          '#type' => 'tableselect',
-          '#header' => $header,
-          '#rows' => $output[0][1],
-          '#empty' => t('No users found'),
-      ];
-
-    return $form;
-  }
+        return $form;
+    }
 
 }
