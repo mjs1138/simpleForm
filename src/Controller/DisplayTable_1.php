ae921b896255168a5e967f03645e58df38c6fbcc
Mike <mike@scllc.us>
2017-03-03 16:07:34 -0500

    Found error: Use class that extends FormBase

--- src/Controller/DisplayTable_1.php
+++ src/Controller/DisplayTable_1.php
@@ -2,35 +2,76 @@
 
 namespace Drupal\simpleForm\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Simple page controller for drupal.
  */
-class DisplayTable_1 extends ControllerBase
+class DisplayTable_1 extends FormBase
 {
-
-    public function description_one()
+    public function buildForm(array $form, FormStateInterface $form_state)
     {
-        $header = [
-            'first_name' => $this->t('First Name'),
-            'last_name' => $this->t('Last Name'),
-        ];
 
+        // TableSelect.
         $options = [
             1 => ['first_name' => 'Indy', 'last_name' => 'Jones'],
             2 => ['first_name' => 'Darth', 'last_name' => 'Vader'],
             3 => ['first_name' => 'Super', 'last_name' => 'Man'],
         ];
 
-        $form['table'] = array(
+        $header = [
+            'first_name' => t('First Name'),
+            'last_name' => t('Last Name'),
+        ];
+
+        $form['table'] = [
             '#type' => 'tableselect',
+            '#title' => $this->t('Users'),
             '#header' => $header,
             '#options' => $options,
-            '#empty' => $this->t('No users found!!!'),
-        );
+            '#empty' => t('No users found'),
+        ];
+
+        // Add a submit button that handles the submission of the form.
+        $form['actions']['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Submit'),
+            '#description' => $this->t('Submit, #type = submit'),
+        ];
+
 
         return $form;
+
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormId() {
+        return 'fapi_example_input_demo_form';
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        // Find out what was submitted.
+        $values = $form_state->getValues();
+        foreach ($values as $key => $value) {
+            $label = isset($form[$key]['#title']) ? $form[$key]['#title'] : $key;
+
+            // Many arrays return 0 for unselected values so lets filter that out.
+            if (is_array($value)) {
+                $value = array_filter($value);
+            }
+
+            // Only display for controls that have titles and values.
+            if ($value && $label) {
+                $display_value = is_array($value) ? preg_replace('/[\n\r\s]+/', ' ', print_r($value, 1)) : $value;
+                $message = $this->t('Value for %title: %value', array('%title' => $label, '%value' => $display_value));
+                drupal_set_message($message);
+            }
+        }
     }
 
 }
